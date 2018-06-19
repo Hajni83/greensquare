d9ef4a3273828fd6084e07d7268371103d9915b0
József Tar <szurilo@gmail.com>
2018-06-19 22:52:42 +0200

    ready

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-
-var canvas=document.getElementById('MyCanvas');
-var context=canvas.getContext('2d');
-context.fillStyle='orange';
-context.fillRect(80,80,80,80);
+$(function() {
+    var canvas=document.getElementById('MyCanvas');
+    var context=canvas.getContext('2d');
+    context.fillStyle='green';
+    var canvasWidth=canvas.width;
+    var canvasHeight=canvas.height;
+    var squareWidth=80;
+    var squareHeight=80;
+    var push=10;
+    context.fillRect(canvasWidth-squareWidth-push,canvasHeight-squareHeight-push, squareWidth, squareHeight);
+  });
 
